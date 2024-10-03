c8cc6403a02fef93b769123b72841181e6d0a5d2
Jenny273-alt <naungayanjenny90@gmail.com>
2024-10-03 10:12:55 +0700

    evenNumbers.js

---
+++ evenNumbers.js
@@ -0,0 +1,11 @@
+// Print all numbers from 1 to 10
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+  }
+  
+  // Print only even numbers from 1 to 10
+  for (let i = 2; i <= 10; i += 2) {
+    console.log(i);
+  }
+
+  //https://l.facebook.com/l.php?u=https%3A%2F%2Fdeveloper.mozilla.org%2Fen-US%2Fdocs%2FWeb%2FJavaScript%2FReference%2FOperators%2FComparison_Operators%3Ffbclid%3DIwZXh0bgNhZW0CMTAAAR158GiRkjSIWGmd_GFcKphLnxgTocMG8kXjY7DrOjZUJMt2ARYRnz-5OSY_aem_GBVf-oJNnXWovhELtAQNOA&h=AT2xCctkMWnkFVePGDbdDI68qIKYVqGlYQRrtrM914zDhbthXGeupfHLh-Rv50u2LksU3knMEMIC_0y9RJAqc6MAYnEqvgie8bAQFY4LgXvfGOhJH0Gl-IPDoZ5r69-J8TZMRA
